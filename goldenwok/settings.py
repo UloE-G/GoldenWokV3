5778e6a1193af636c4058966805945d73b9b2449
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-06-28 10:55:55 +0000

    Created Superuser

--- goldenwok/settings.py
+++ goldenwok/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=@_1x_alfbm^yrdmbpym+u-u@krc_l9vus7ulyib7_r616aj#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-uloeg-goldenwokv3-8xw9o9ql63p.ws.codeinstitute-ide.net'
-                ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-uloeg-goldenwokv3-8xw9o9ql63p.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
@@ -75,13 +79,16 @@ WSGI_APPLICATION = 'goldenwok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
