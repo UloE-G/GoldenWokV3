3f24488eb15f27fdbd65c31f321828bfa30b60c9
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-06-30 18:55:40 +0000

    Created template

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+# Post model 
+admin.site.register(Post)
+
+
+# Comment model
+admin.site.register(Comment)
