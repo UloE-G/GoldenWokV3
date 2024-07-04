79f62d906924f95dccaaf55de2287b109ad422c5
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-04 11:14:59 +0000

    Changed debug to true

--- goldenwok/settings.py
+++ goldenwok/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-uloeg-goldenwokv3-8xw9o9ql63p.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
