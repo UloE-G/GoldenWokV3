0bfdf49c7e338e49c035c0d6e2d137af7452e17d
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-15 10:16:05 +0000

    Made the request to set the post id directly

--- about/views.py
+++ about/views.py
@@ -18,31 +18,34 @@ def post_list_and_review(request, slug=None):
         comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
+            print("Valid form")
             comment = comment_form.save(commit=False)
             comment.author = request.user
-            comment.post = post
-            # comment.save()
+            comment.post_id = request.POST.get('post')  # Set the post_id directly
+            comment.save()
             messages.add_message(
             request, messages.SUCCESS,
             'Comment submitted and awaiting approval'
             )
 
-    if not comment_form:
-        comment_form = CommentForm()
+    
+    comment_form = CommentForm()
+    print("About to render template")
 
-    context = {
-        "post": post,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
-    }
+    # context = {
+    #     "post": post,
+    #     "comments": comments,
+    #     "comment_count": comment_count,
+    #     "comment_form": comment_form,
+    # }
 
-    if post:
-        template_name = "about/index.html"
-    else:
-        template_name = "about/other_template.html"
+    # if post:
+    #     template_name = "about/index.html"
+    # else:
+    #     template_name = "about/other_template.html"
 
     return render(
         request, 
