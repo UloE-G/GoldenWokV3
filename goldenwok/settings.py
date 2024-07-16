aa8e4faf536dae9927bf8fb1d7d08921f233a60c
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-16 10:02:08 +0000

    Changed Databases back to os.environ

--- goldenwok/settings.py
+++ goldenwok/settings.py
@@ -98,16 +98,16 @@ WSGI_APPLICATION = 'goldenwok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-uloeg-goldenwokv3-8xw9o9ql63p.ws.codeinstitute-ide.net',
