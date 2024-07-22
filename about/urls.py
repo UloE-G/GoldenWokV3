6a769d80ac7942fa66e4d1d525c92872fff6efae
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-22 20:27:25 +0000

    Created paths for edit and delete comments

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.post_list_and_review, name='about'),
+    path('edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'),
+    path('delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'),
 ]
