e7af827b7fd8b54d3b86d65b94f8fb5ef4b4e2c9
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-26 10:06:05 +0000

    Testing for contact section

---
+++ contact/tests_views.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Contact
+from .forms import CollaborateForm
+
+
+class TestAboutView(TestCase):
+
+    def setUp(self):
+        """Creates Contact content"""
+        self.contact_content = Contact(
+            title="Contact", content="Contact page.")
+        self.contact_content.save()
+
+    def test_render_about_page_with_collaborate_form(self):
+        """Verifies get request for about me containing a collaboration form"""
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Contact', response.content)
+        self.assertIsInstance(
+            response.context['collaborate_form'], CollaborateForm)
