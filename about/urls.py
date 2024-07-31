f0567ce8750fe1f7f78e948f1523a6d8bd62aace
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-31 10:11:25 +0000

    Updated code from python validator

--- about/urls.py
+++ about/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.post_list_and_review, name='about'),
     path('edit_comment/<int:comment_id>',
-        views.comment_edit, name='comment_edit'),
+         views.comment_edit, name='comment_edit'),
     path('delete_comment/<int:comment_id>',
-        views.comment_delete, name='comment_delete'),
+         views.comment_delete, name='comment_delete'),
 ]
