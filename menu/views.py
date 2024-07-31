a57afde39551ab80bbad2eb303214f0ce476ba16
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-07-31 09:34:09 +0000

    Removed menu from admin

--- menu/views.py
+++ menu/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from .models import Menu
+# from .models import Menu
 
 # Create your views here.
+
 
 def my_menu(request):
     """
     Renders the Menu page
     """
-
-    menu = Menu.objects.all().order_by('-updated_on').first()
-
     return render(
         request,
         "menu/menu.html",
-        {"menu": menu},
     )
